e34f2ab62e74249a03d34e7ea17f6bb6870ef9fc
Sarang-R-119 <44112750+Sarang-R-119@users.noreply.github.com>
2022-01-07 00:11:38 -0800

    Time: 23 ms, Memory: 12.3 MB - LeetHub

--- two-sum/two-sum.cpp
+++ two-sum/two-sum.cpp
@@ -1,25 +1,44 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+      
+    map<int, int> diff;
+    vector<int> indices;
         
-        
-    map<int, int> map1; // Create map for storing the differences of target and value in array
-
-    for (int i = 0; i < nums.size(); i++)
-    {
-        map1.insert(pair<int, int>(target - nums[i], i));
-    } // Do calculations
-
-    for (int j = 0; j < nums.size(); j++)
-    {
-        map<int,int>::iterator iter = map1.find(nums[j]); // Check to find if difference is a value in array
-        if(iter != map1.end() && iter->second != j) // If difference is present and indices are not equal, return a vector of those indices
+    for (int i = 0; i < nums.size(); i++){
+        diff[target - nums[i]] = i;
+    }
+    
+    for (int i = 0; i < nums.size(); i++){
+        if(diff.find(nums[i]) != diff.end() && diff[nums[i]] != i)
         {
-            return vector<int>{iter->second, j};
+            indices = {diff[nums[i]], i};
+            break;
         }
     }
-
-    return vector<int>();
-
+    return indices;
     }
 };
+    
+    
+    
+        
+//     map<int, int> map1; // Create map for storing the differences of target and value in array
+
+//     for (int i = 0; i < nums.size(); i++)
+//     {
+//         map1.insert(pair<int, int>(target - nums[i], i));
+//     } // Do calculations
+
+//     for (int j = 0; j < nums.size(); j++)
+//     {
+//         map<int,int>::iterator iter = map1.find(nums[j]); // Check to find if difference is a value in array
+//         if(iter != map1.end() && iter->second != j) // If difference is present and indices are not equal, return a vector of those indices
+//         {
+//             return vector<int>{iter->second, j};
+//         }
+//     }
+
+//     return vector<int>();
+
+//     }
