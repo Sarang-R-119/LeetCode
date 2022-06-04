6eea80da598f09d3f15f0f4169766e39f26755da
Sarang-R-119 <44112750+Sarang-R-119@users.noreply.github.com>
2022-06-04 15:56:24 -0400

    Time: 0 ms (100.00%), Space: 6 MB (69.98%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     string defangIPaddr(string address) {
+
+        string newIP;
+        string:: iterator it;
+        for (it = address.begin(); it != address.end(); it++){
+            if(*it == '.')
+                newIP += "[.]";
+            else
+                newIP += *it;
+        }
+        
+/*        O(n^2)
         std::size_t found = 0;
         
         while(address.find('.', found) != string::npos){
@@ -9,7 +20,8 @@ public:
             address.insert(found + 2,"]");
             found += 2;
         }
+*/
         
-        return address;
+        return newIP;
     }
 };
