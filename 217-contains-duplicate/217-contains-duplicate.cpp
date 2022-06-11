7c4448bc1dda6346eee37822a1ac6de4c6d22490
Sarang-R-119 <44112750+Sarang-R-119@users.noreply.github.com>
2022-06-11 14:50:43 -0400

    Time: 116 ms (66.49%), Space: 46.6 MB (91.62%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        // bf
+        /*for (int i = 0; i < nums.size(); i++){
+            for (int j = i+1; j < nums.size(); j++){
+                if(nums[i] == nums[j]){
+                    return true;
+                }
+            }
+        }*/
+        
+        // sorted
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size() - 1; i++){
+            if(nums[i] == nums[i+1])
+                return true;
+        }
+        
+        return false;
+    }
+};
