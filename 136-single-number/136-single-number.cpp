ac76fdcd108ecc2eb321481ac49c32bfcd13b0b2
Sarang-R-119 <44112750+Sarang-R-119@users.noreply.github.com>
2022-06-12 19:10:31 -0400

    Time: 73 ms (5.13%), Space: 20.7 MB (6.65%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        //Hashmap
+        map<int, int> dict;
+        
+        for(int i = 0; i < nums.size(); i++){
+            if(dict.find(nums[i]) != dict.end()){
+                dict[nums[i]] += 1;
+            }else dict[nums[i]] = 1;
+        }
+        for(int i = 0; i < nums.size(); i++){
+            if(dict[nums[i]] == 1)
+                return nums[i];
+        }
+        return 0;
+    }
+};
